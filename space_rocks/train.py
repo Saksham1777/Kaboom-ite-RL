6ff1afcaf71c367e455de4414a88a53b3b9cc59e
Saksham1777 <sakshamgupta1217@gmail.com>
2026-03-28 00:02:32 +0530

    RL training - version 4
    - Better turning for bigger turn
    - Smaller turns need tuning since too much shaky movement
    - Survives for longer episodes than previous versions
    - Higher average reward than previous versions

--- space_rocks/train.py
+++ space_rocks/train.py
@@ -40,7 +40,7 @@ class SpaceRocksCallback(BaseCallback):
 
 # MAIN TRAIN LOOP
 
-version = 3
+version = 4
 
 log_dir = "./logs/spacerocks_tensorboard/"
 os.makedirs(log_dir, exist_ok=True)
@@ -61,7 +61,7 @@ model = PPO(
 # 4. Train with the Callback
 print("Training started. Open TensorBoard to see results.")
 model.learn(
-    total_timesteps=300000, 
+    total_timesteps=200000, 
     callback=SpaceRocksCallback(),
     tb_log_name=f"PPO_run_{version}"
 )
